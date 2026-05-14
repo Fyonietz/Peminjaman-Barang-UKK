ed8402a883c9153715b985930e54b31932257cdf
Fyonietz <expduta@gmail.com>
2026-05-14 17:36:34 +0700

    feat(frontend):init astro

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -37,9 +37,6 @@ namespace Backend.Controllers{
           }
       });
 
-      g.MapGet("/test",async()=>{
-        return Results.Ok(new {message = "SIpp"});
-      }).RequireAuthorization(Policies.Admin);
     }
   }
 }
